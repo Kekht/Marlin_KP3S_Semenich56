53bbc66f56736c46a3b0bf2c01945b9008566af7
Kekht <telegram@kekht>
2023-11-25 21:29:22 +0300

    Y axis current fix

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2883,7 +2883,7 @@
   #endif
 
   #if AXIS_IS_TMC_CONFIG(Y)
-    #define Y_CURRENT       800
+    #define Y_CURRENT       950
     #define Y_CURRENT_HOME  Y_CURRENT
     #define Y_MICROSTEPS     16
     #define Y_RSENSE          0.11
